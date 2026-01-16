9baebec6aff5a4e1ebc968eb5815cf93d295fcbc
samugoha <samgomhay@alu.edu.gva.es>
2026-01-16 12:26:40 +0100

    ejercicios powershell

---
+++ ejercicio2_practica2.ps1
@@ -0,0 +1,15 @@
+﻿
+[int]$numero1 = Read-Host "Escribe el primer número"
+[int]$numero2 = Read-Host "Escribe el segundo número"
+
+$suma = $numero1 + $numero2
+$resta = $numero1 - $numero2
+$multiplicacion = $numero1 * $numero2
+$division = $numero1 / $numero2
+$resto = $numero1 % $numero2
+
+Write-Host "La suma es: $suma"
+Write-Host "La resta es: $resta"
+Write-Host "La multiplicación es: $multiplicacion"
+Write-Host "La división es: $division"
+Write-Host "El resto es: $resto"
---
+++ ejercicio3_practica2.ps1
@@ -0,0 +1,6 @@
+﻿[int]$horas = Read-Host "Escribe el número de horas que has trabajado"
+[float]$costeHora = Read-Host "Escribe el coste de la hora"
+
+$salario = $horas * $costeHora
+
+Write-Host "El salario que se debe pagar es: $salario"
