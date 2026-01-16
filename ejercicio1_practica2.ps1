aa81a0074f67986fbd89f196b6485f64014a7f77
samugoha <samgomhay@alu.edu.gva.es>
2026-01-16 12:17:18 +0100

    ejercicio powershell

---
+++ ejercicio1_practica2.ps1
@@ -0,0 +1,10 @@
+﻿
+$nombreUsuario = $env:USERNAME          
+$directorioTrabajo = $env:USERPROFILE   
+$nombreEquipo = $env:COMPUTERNAME      
+$nombreDominio = "Nombre_dominio"       
+
+Write-Host "Hola $nombreUsuario"
+Write-Host "Tu directorio de trabajo es $directorioTrabajo"
+Write-Host "Perteneces al dominio $nombreDominio"
+Write-Host "Tu equipo se llama $nombreEquipo"
